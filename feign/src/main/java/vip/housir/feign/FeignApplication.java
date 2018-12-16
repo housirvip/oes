e2a8830952a6ed9924ade2435ae7f2c206ac19c4
二营长的意大利兄弟 <housirvip@outlook.com>
2018-12-16 23:56:26 +0800

    rm data source

--- feign/src/main/java/vip/housir/feign/FeignApplication.java
+++ feign/src/main/java/vip/housir/feign/FeignApplication.java
@@ -2,15 +2,17 @@ package vip.housir.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author housirvip
  */
-@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class FeignApplication {
 
     public static void main(String[] args) {
