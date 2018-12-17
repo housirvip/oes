a7d0fc213ac857d5858139d9ec5040871407da72
可以提交但没必要 <housirvip@outlook.com>
2018-12-17 11:42:39 +0800

    update

--- user/src/main/java/vip/housir/user/UserApplication.java
+++ user/src/main/java/vip/housir/user/UserApplication.java
@@ -3,12 +3,10 @@ package vip.housir.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author housirvip
  */
-@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"vip.housir.user", "vip.housir.base"})
 public class UserApplication {
