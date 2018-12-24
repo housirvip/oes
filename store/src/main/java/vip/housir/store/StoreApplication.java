72e46bebdce63efc3267f47a40cd467d206dba5b
可以提交但没必要 <housirvip@outlook.com>
2018-12-24 14:29:38 +0800

    add store module

---
+++ store/src/main/java/vip/housir/store/StoreApplication.java
@@ -0,0 +1,17 @@
+package vip.housir.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author housirvip
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"vip.housir.store", "vip.housir.base"})
+public class StoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
+}
