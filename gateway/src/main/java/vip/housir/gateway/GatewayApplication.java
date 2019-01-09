4050382c9f733b83193d5607583fef50b6007f33
可以提交但没必要 <housirvip@outlook.com>
2019-01-09 13:53:46 +0800

    update

--- gateway/src/main/java/vip/housir/gateway/GatewayApplication.java
+++ gateway/src/main/java/vip/housir/gateway/GatewayApplication.java
@@ -2,13 +2,15 @@ package vip.housir.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author housirvip
  */
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class GatewayApplication {
 
     public static void main(String[] args) {
