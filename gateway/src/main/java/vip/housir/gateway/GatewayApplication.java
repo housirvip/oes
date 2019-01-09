283cf0b30fc3f1878084f6136afed09aee25068f
可以提交但没必要 <housirvip@outlook.com>
2019-01-09 13:47:16 +0800

    add gateway module

---
+++ gateway/src/main/java/vip/housir/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package vip.housir.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author housirvip
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
