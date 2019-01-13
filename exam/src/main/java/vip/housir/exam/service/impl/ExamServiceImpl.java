d8f23ec47c1f993b3621eb961295cbbce1c06570
保温杯里泡枸杞 <housirvip@outlook.com>
2019-01-13 23:17:36 +0800

    update async score for exam

--- exam/src/main/java/vip/housir/exam/service/impl/ExamServiceImpl.java
+++ exam/src/main/java/vip/housir/exam/service/impl/ExamServiceImpl.java
@@ -2,6 +2,7 @@ package vip.housir.exam.service.impl;
 
 import com.github.pagehelper.Page;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import vip.housir.base.constant.ErrorMessage;
 import vip.housir.base.dto.PageDto;
 import vip.housir.exam.entity.Exam;
+import vip.housir.exam.entity.Paper;
+import vip.housir.exam.entity.Question;
+import vip.housir.exam.entity.Section;
 import vip.housir.exam.mapper.ExamMapper;
 import vip.housir.exam.mapper.PaperMapper;
 import vip.housir.exam.mapper.QuestionMapper;
@@ -17,8 +21,12 @@ import vip.housir.exam.mapper.SectionMapper;
 import vip.housir.exam.mqhandler.ExamOutput;
 import vip.housir.exam.service.ExamService;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author housirvip
@@ -79,57 +87,48 @@ public class ExamServiceImpl implements ExamService {
 
     @Override
     public void score(Integer id) {
-//
-//        Exam exam = examMapper.selectByPrimaryKey(id);
-//        Preconditions.checkNotNull(exam, ErrorMessage.EXAM_NOT_FOUND);
-//
-//        Paper paper = paperMapper.selectByPrimaryKey(exam.getPid());
-//        Preconditions.checkNotNull(paper, ErrorMessage.PAPER_NOT_FOUND);
-//
-//        Map<String, Double> sectionScore = Maps.newHashMap();
-//        Map<Integer, Section> sectionMap = sectionMapper.listInIds(paper.getSids());
-//        paper.getSids().forEach(sid -> {
-//
-//            //thisScore 为此 section 得分
-//            AtomicReference<Float> thisScore = new AtomicReference<>(0f);
-//            Section section = sectionMap.get(sid);
-//            if (section == null) {
-//                return;
-//            }
-//
-//            float everyScore = section.getTotalScore() / section.getQids().size();
-//            Map<Integer, Question> questionMap = questionMapper.listInIds(section.getQids());
-//            section.getQids().forEach(qid -> {
-//
-//                Question question = questionMap.get(qid);
-//                if (question == null) {
-//                    return;
-//                }
-//
-//                String answer = exam.getUserAnswer().get(question.getId().toString());
-//                if (answer == null || question.getAnswer() == null) {
-//                    return;
-//                }
-//                if (answer.equals(question.getAnswer())) {
-//                    //答对加分
-//                    thisScore.updateAndGet(v -> v + everyScore);
-//                } else if (section.getDeduct()) {
-//                    //答错扣分，如果 section 开启了 deduct
-//                    thisScore.updateAndGet(v -> v - everyScore);
-//                }
-//            });
-//            if (thisScore.get() < 0) {
-//                //不允许负分
-//                thisScore.set(0f);
-//            }
-//            BigDecimal finalScore = new BigDecimal(thisScore.get()).setScale(2, BigDecimal.ROUND_HALF_UP);
-//            thisScore.set(finalScore.floatValue());
-//            sectionScore.put(section.getId().toString(), finalScore.doubleValue());
-//        });
-//        sectionScore.forEach((k, v) -> exam.setScore(exam.getScore() + v.floatValue()));
-//        exam.setSectionScore(sectionScore);
-//        exam.setUserAnswer(null);
-//
-//        examMapper.updateByPrimaryKeySelective(exam);
+
+        Exam exam = examMapper.selectByPrimaryKey(id);
+        Preconditions.checkNotNull(exam, ErrorMessage.EXAM_NOT_FOUND);
+
+        Paper paper = paperMapper.selectByPrimaryKey(exam.getPid());
+        Preconditions.checkNotNull(paper, ErrorMessage.PAPER_NOT_FOUND);
+
+        Map<String, Double> sectionScore = Maps.newHashMap();
+        List<Section> sectionList = sectionMapper.listByPid(paper.getId());
+        sectionList.forEach(section -> {
+
+            //thisScore 为此 section 得分
+            AtomicReference<Float> thisScore = new AtomicReference<>(0f);
+
+            List<Question> questionList = questionMapper.listBySid(section.getId());
+            float everyScore = section.getTotalScore() / questionList.size();
+            questionList.forEach(question -> {
+
+                String answer = exam.getUserAnswer().get(question.getId().toString());
+                if (answer == null || question.getAnswer() == null) {
+                    return;
+                }
+                if (answer.equals(question.getAnswer())) {
+                    //答对加分
+                    thisScore.updateAndGet(v -> v + everyScore);
+                } else if (section.getDeduct()) {
+                    //答错扣分，如果 section 开启了 deduct
+                    thisScore.updateAndGet(v -> v - everyScore);
+                }
+            });
+            if (thisScore.get() < 0) {
+                //不允许负分
+                thisScore.set(0f);
+            }
+            BigDecimal finalScore = new BigDecimal(thisScore.get()).setScale(2, BigDecimal.ROUND_HALF_UP);
+            thisScore.set(finalScore.floatValue());
+            sectionScore.put(section.getId().toString(), finalScore.doubleValue());
+        });
+        sectionScore.forEach((k, v) -> exam.setScore(exam.getScore() + v.floatValue()));
+        exam.setSectionScore(sectionScore);
+        exam.setUserAnswer(null);
+
+        examMapper.updateByPrimaryKeySelective(exam);
     }
 }
