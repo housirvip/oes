73cc57696692246281e899e957d82e49f1755290
可以提交但没必要 <housirvip@outlook.com>
2019-01-14 17:21:42 +0800

    add only one user login check //todo

--- gateway/src/main/java/vip/housir/gateway/GatewayConfig.java
+++ gateway/src/main/java/vip/housir/gateway/GatewayConfig.java
@@ -1,5 +1,6 @@
 package vip.housir.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -16,6 +17,7 @@ import reactor.core.publisher.Mono;
 /**
  * @author: housirvip
  */
+@Slf4j
 @Configuration
 public class GatewayConfig {
 
@@ -38,6 +40,21 @@ public class GatewayConfig {
                     response.setStatusCode(HttpStatus.OK);
                     return Mono.empty();
                 }
+            }
+            return chain.filter(ctx);
+        };
+    }
+
+    @Bean
+    public WebFilter jwtFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            String uid = request.getHeaders().getFirst("X-U-ID");
+            String auth = request.getHeaders().getFirst("Authorization");
+            if (uid != null && auth != null) {
+                //TODO 单一用户登录检查
+                log.info(uid);
+                log.info(auth);
             }
             return chain.filter(ctx);
         };
